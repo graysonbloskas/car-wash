aa56ac45a3f424eb1b233c3fe70b8d13a25ff41f
Grayson Bloskas <graysonbloskas@Graysons-iMac-Pro.local>
2021-09-07 14:20:50 -0400

    file structure, working on Navbar and Homepage

---
+++ src/components/Navbar.js
@@ -0,0 +1,140 @@
+// import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+// import Logo from '../assets/images/logo.png';
+// import './Navbar.css';
+
+// function Navbar() {
+//   const [click, setClick] = useState(false);
+//   const [button, setButton] = useState(true);
+
+//   const handleClick = () => setClick(!click);
+//   const closeMobileMenu = () => setClick(false)
+
+//   const showButton = () => {
+//     if (window.innerWidth <= 960) {
+//       setButton(false);
+//     } else {
+//       setButton(true);
+//     }
+//   };
+
+//   useEffect(() => {
+//     showButton();
+//   }, []);
+
+//   window.addEventListener('resize', showButton);
+
+//   return (
+//     <>
+//       <nav className='navbar'>
+//         <div className='navbar-container'>
+//           <Link to='/' className='navbar-logo'>
+//             <img src={Logo} alt="Sam and Grayson's Logo" className='logo' />
+//           </Link>
+//           <div className='menu-icon' onClick={handleClick}>
+//             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+//           </div>
+//           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+//             <li className="nav-item">
+//               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+//                 Home
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+//                 Services
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+//                 Booking
+//               </Link>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//     </>
+//   );
+// }
+
+// export default Navbar;
+
+
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/images/logo.png';
+import './Navbar.css';
+
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            <img src={Logo} alt="" className='logo'/>
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/locations'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Locations
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar'    
+import '../App.css';
+import './Homepage.css'
+
+function Homepage() {
+    return (
+        <>
+        <Navbar />
+        </>
+    )
+}
+
+export default Homepage
