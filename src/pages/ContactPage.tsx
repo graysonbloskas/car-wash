4536b372b201bb2d63fbd2ac98bd60b63f910177
Grayson Bloskas <graysonbloskas@Graysons-iMac-Pro.local>
2021-09-08 19:32:24 -0400

    contact form completely finished, for now. Later, will be adding more relevant input fields

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,6 +1,7 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState, useRef } from 'react';
 import formId from '../config';
 import axios from 'axios';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 type FormState = {
   email: string;
@@ -15,6 +16,8 @@ type ServiceMessage = {
 
 function ContactPage() {
   const formSparkUrl = `https://submit-form.com/${formId}`;
+  const recaptchaKey = '6LcEL1QcAAAAAE6PkTRYWnI0N9Cuo1YQDyor616e';
+  const recaptchaRef = useRef<any>();
 
   const initialFormState = {
     email: '',
@@ -25,6 +28,7 @@ function ContactPage() {
   const [formState, setFormState] = useState<FormState>(initialFormState);
   const [submitting, setSubmitting] = useState<boolean>(false);
   const [message, setMessage] = useState<ServiceMessage>();
+  const [recaptchaToken, setRecaptchaToken] = useState<string>();
 
   const submitForm = async (event: FormEvent) => {
     event.preventDefault();
@@ -36,15 +40,18 @@ function ContactPage() {
   const postSubmission = async () => {
     const payload = {
       ...formState,
+      'g-recaptcha-response': recaptchaToken,
     };
 
     try {
       const result = await axios.post(formSparkUrl, payload);
       console.log(result);
       setMessage({
-        class: 'bg-green-500',
+        class: 'font-bold text-lg my-2 bg-gradient-to-r from-green-300 to-green-400 text-white',
         text: 'Thanks, We will be in touch shortly',
       });
+      setFormState(initialFormState);
+      recaptchaRef.current.reset()
     } catch (error) {
       console.log(error);
       setMessage({
@@ -62,6 +69,10 @@ function ContactPage() {
     const updatedFormState = { ...formState };
     updatedFormState[formKey] = value;
     setFormState(updatedFormState);
+  };
+
+  const updateRecaptchaToken = (token: string | null) => {
+    setRecaptchaToken(token as string);
   };
   return (
     <div className='flex items-center justify-center h-screen flex-col'>
@@ -84,16 +95,18 @@ function ContactPage() {
               className='border-2 p-2'
               id='name'
               value={formState.name}
+              required
             />
           </div>
           <div className='my-2 flex flex-col'>
             <label htmlFor='email'>Email</label>
             <input
               onChange={updateFormControl}
-              type='text'
+              type='email'
               className='border-2 p-2'
               id='email'
               value={formState.email}
+              required
             />
           </div>
           <div className='my-2 flex flex-col'>
@@ -105,6 +118,13 @@ function ContactPage() {
               value={formState.message}
             ></textarea>
           </div>
+
+          <ReCAPTCHA
+            ref={recaptchaRef}
+            sitekey={recaptchaKey}
+            onChange={updateRecaptchaToken}
+          ></ReCAPTCHA>
+
           <button
             disabled={submitting}
             className='font-bold text-lg my-2 bg-gradient-to-r from-green-300 to-green-400  text-white w-full p-2 hover:from-green-600 hover:to-green-600 transition-colors duration-200 '
